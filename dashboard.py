3f1306f286acfcde9bc572b92eb8bd735ea72190
zaclake <zlake@bluegreenbiogas.com>
2024-11-07 15:35:08 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -55,8 +55,9 @@ def run_dashboard():
     # Extract variables for visualization
     unit_process_scores = plant_scores.get('unit_process_scores', {})
     formatted_unit_process_names = plant_scores.get('formatted_unit_process_names', {})
+    st.write("plant_scores:", plant_scores)
     data_completeness = plant_scores.get('data_completeness', pd.Series(dtype='float64'))
-
+    
     # Calculate scores over time
     scores_over_time = scoring.calculate_scores_over_time(ml_data, configuration)
     if scores_over_time is None:
