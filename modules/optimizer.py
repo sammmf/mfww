38ca5920ba88e3c728d3a026e6299172afc204fc
zaclake <zlake@bluegreenbiogas.com>
2024-11-12 17:01:33 -0500

    Update optimizer.py

--- modules/optimizer.py
+++ modules/optimizer.py
@@ -223,9 +223,9 @@ def optimize_process(
             objective_function,
             bounds=bounds,
             strategy='best1bin',
-            maxiter=150,
+            maxiter=200,
             popsize=5,
-            tol=0.075,
+            tol=0.05,
             mutation=(0.5, 1),
             recombination=0.7,
             disp=False
